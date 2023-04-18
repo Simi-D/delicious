8713b4e6e5ddcb99a4a1ef39940c93d7a4eec7d7
Simi Dijith <simi_koras@hotmail.com>
2023-04-18 13:23:19 +0530

    final

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Home from "./Home";
+import Searched from "../components/Searched";
 import Cuisine from "./Cuisine";
 import { Route, Routes } from "react-router-dom";
+import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cuisine/:type" element={<Cuisine />} />
-    </Routes>
+    <AnimatePresence>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
